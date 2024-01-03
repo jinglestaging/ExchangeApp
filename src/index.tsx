4c5595ae09df4669ca61940640d4b0760d2daad9
Lukas Hodac <zandom@seznam.cz>
2024-01-03 17:53:30 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+import Shell from './components/shell'
+import GlobalStyle from './lib/global-style'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <QueryClientProvider client={queryClient}>
+    <GlobalStyle />
+    <Shell />
+  </QueryClientProvider>,
+)
